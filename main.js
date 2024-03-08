b9712ee2a8e0a8277a779b49530e4a8a85a3beed
Casamiento-Ivan-Caro <161479973+Casamiento-Ivan-Caro@users.noreply.github.com>
2024-03-08 20:30:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -30,3 +30,31 @@ simplyCountdown('#Contador', {
 });
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    let audio = document.getElementById("myAudio");
+    let playButton = document.getElementById("playButton");
+
+
+    playButton.addEventListener("click", function () {
+        if (audio.paused) {
+            audio.play();
+            playButton.textContent = "Pausar";
+        } else {
+            audio.pause();
+            playButton.textContent = "Play";
+        }
+
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
